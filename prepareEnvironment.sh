5026865030d4ff26e9c3e803eba6a491f3727da0
Luis Dario Simonassi <ldsimonassi@gmail.com>
2012-06-10 22:05:45 -0300

    delete extra file

--- prepareEnvironment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -9 node
-nohup node ./src/test/node/mock-feed.js ./src/test/node/sequence.mock 1> ./logs/mock-feed.log 2> ./logs/mock-feeds.err &
-nohup node ./src/test/node/mock-api-server 1> ./logs/mock-api.log 2> ./logs/mock-api.err &
-echo "End."
